4ee1dc693379948f6baa9d77d68f0431d485cb47
minl <Minl@MinlMacBook-Pro.local>
2016-09-03 15:51:47 +0800

    v-model模型指令，新增待办事项

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h1>{{title}}</h1>
     <!-- <h1 v-text="title"></h1> 和上面效果相同-->
+    <!-- 绑定回车事件 -->
+    <input v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
     <!-- 指令v-for用于遍历数据对象中的值；指令v-bind:class 绑定一个样式对象，键为样式类名，值为数据对象中的属性布尔值，用来控制是否加载.finished样式;v-on:click为事件指令-->
       <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
@@ -18,21 +20,20 @@ export default {
   data:function(){
     return {
       title:'这是一个待办事项列表',
-      items:[
-        {
-          label:'看书',
-          isFinished:true
-        },
-        {
-          label:'打球',
-          isFinished:false
-        }
-      ]
+      items:[],
+      newItem:''
     }
   },
   methods:{
     toggleFinish:function(item){
      item.isFinished=!item.isFinished;//属性取反
+    },
+    addNew:function(){
+     this.items.push({//将新增的newItem对象添加到items数组中
+      label:this.newItem,
+      isFinished:false
+     });
+      this.newItem = '';//清空输入框newItem模型
     }
   }
 }
